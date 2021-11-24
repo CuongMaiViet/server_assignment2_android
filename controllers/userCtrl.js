c069378a1917b8ae4e3f114e2a17fc2a6d276ba9
Cuong <s3682365@SGD-93146SR.RMIT.EDU.VN>
2021-11-24 14:57:26 +0700

    add getUser info router and controller

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -8,11 +8,14 @@ const userCtrl = {
 
             const user = await Users.findOne({ email })
 
-            if (user) 
-            return res.status(400).json({ msg: 'The email is already exists.' })
+            if (user)
+                return res.status(400).json({ msg: 'The email is already exists.' })
 
-            if (password.length < 6) 
-            return res.status(400).json({ msg: 'Password need 6 character.' })
+            if (password.length < 6)
+                return res.status(400).json({ msg: 'Password need 6 character.' })
+
+            if (!name)
+                return res.status(400).json({ msg: 'Please fill all the requirement.' })
 
             const hashPW = await bcrypt.hash(password, 10)
 
@@ -24,8 +27,7 @@ const userCtrl = {
 
             await newUser.save()
 
-            res.json({ msg: 'Register successfully' })
-            // res.json({ password, hashPW })
+            return res.json({ msg: 'Register successfully' })
 
         } catch (error) {
             return res.status(500).json({ msg: error.message })
@@ -46,13 +48,28 @@ const userCtrl = {
                 return res.status(400).json({ msg: "Password does not correct" })
 
             return res.json({
-                msg:`Login successfully as ${email}`,
+                msg: `Login successfully as ${email}`,
                 result: user._id,
             })
+
         } catch (error) {
             return res.status(500).json({ msg: error.message })
-        }ß
+        }
     },
+
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findById({ _id: req.params.id })
+
+            if (!user)
+                return res.status(400).json({ msg: 'User does not exist.' })
+
+            return res.json(user)
+
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    }
 }
 
 module.exports = userCtrl
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,6 @@ const userCtrl = require('../controllers/userCtrl')
 
 router.post('/register', userCtrl.register)
 router.post('/login', userCtrl.login)
+router.get('/info', userCtrl.getUser)
 
 module.exports = router
